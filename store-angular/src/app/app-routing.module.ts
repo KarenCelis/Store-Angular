29933438d896c26d9f691a0b33d7f2324835bce0
Karen Celis <k-celis@javeriana.edu.co>
2021-01-06 12:01:00 -0500

    rutas

--- store-angular/src/app/app-routing.module.ts
+++ store-angular/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import {HomeComponent} from './home/home.component';
+import {ProductsComponent} from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '/home',
+  component: HomeComponent},
+
+  
+  {path: '/products',
+  component: ProductsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
