2fd9ff542445da6296f49fe6a2e5a10564391139
Karen Celis <k-celis@javeriana.edu.co>
2021-02-01 11:36:29 -0500

    Create home page with components

--- store-angular/src/app/app-routing.module.ts
+++ store-angular/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { from } from 'rxjs';
 import {HomeComponent} from './home/home.component';
 import {ProductsComponent} from './products/products.component';
 import {ContactComponent} from './contact/contact.component';
-
+import {DemoComponent} from './demo/demo.component';
 const routes: Routes = [
 {
   path: 'home',
@@ -15,7 +15,10 @@ const routes: Routes = [
   component: ProductsComponent},
    
   {path: 'contact',
-  component: ContactComponent}
+  component: ContactComponent},
+
+  {path: 'demo',
+  component: DemoComponent}
 
 ];
 
---
+++ store-angular/src/app/demo/demo.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent implements OnInit {
+  title = 'store-angular';
+
+  items = ['Antu', 'Amaru', 'Morgan'];
+
+  power = 10;
+
+  
+ngOnInit(){
+
+  
+}
+  addItem(){
+  this.items.push('nuevo item');
+
+  }
+  deleteItem(index: number){
+
+    this.items.splice(index,1);
+
+  }
+
+
+}
