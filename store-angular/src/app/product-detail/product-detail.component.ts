11d22ed5b00d9d24950e6495b3c03edf13adcb9c
Karen Celis <k-celis@javeriana.edu.co>
2021-03-20 10:36:09 -0500

    vistas anidadas

--- store-angular/src/app/product-detail/product-detail.component.ts
+++ store-angular/src/app/product-detail/product-detail.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
-import {ProductsService} from './../products.service';
-
+import { ActivatedRoute, Params } from '@angular/router';
+import { ProductsService } from './../products.service';
+import { Product } from './../product.model';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
-
+  product: Product = {} as Product;
   constructor(
-   private route : ActivatedRoute,
-   private productsService : ProductsService
+    private route: ActivatedRoute,
+    private productsService: ProductsService
   ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) =>{
+    this.route.params.subscribe((params: Params) => {
 
       const id = params.id;
-      const product = this.productsService.getProduct(id);
-      console.log(product)
+      this.product = this.productsService.getProduct(id) as Product;
+
     });
   }
 
